34c96e52701fbedb150b9af9cf43734287247ee6
nikitakryaklin <nikitakryaklin2013@gmail.com>
2025-07-25 21:00:34 +0300

    middleware

--- frontend/src/middleware.ts
+++ frontend/src/middleware.ts
@@ -1,45 +1,24 @@
-import { NextResponse } from "next/server";
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  const { url, cookies } = request;
-
   const { pathname } = request.nextUrl;
+  const token = request.cookies.get("token")?.value;
 
-  // Разрешаем доступ к публичным маршрутам
-  const publicPaths = [
-    "/",
-    "/auth/login",
-    "/auth/register",
-    "/favicon.ico",
-    "/_next",
-    "/api",
-  ];
+  const isRoot = pathname === "/";
+  const isAuthPage = pathname.startsWith("/auth");
 
-  const isPublic = publicPaths.some((path) => pathname.startsWith(path));
-
-  if (isPublic) {
-    return NextResponse.next();
+  if (isAuthPage && token) {
+    return NextResponse.redirect(new URL("/account/profile", request.url));
   }
 
-  const sessions = cookies.get("token")?.value;
-  console.log(sessions, "dsa");
-
-  const isAuthPage = pathname.startsWith("/auth/");
-
-  if (isAuthPage) {
-    if (sessions) {
-      return NextResponse.redirect(new URL("/account/profile", url));
-    }
-
-    return NextResponse.next();
+  const isPublic = isRoot || isAuthPage;
+  if (!isPublic && !token) {
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
-  if (!sessions) {
-    return NextResponse.redirect(new URL("/", url));
-  }
+  return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/:path*"],
+  matcher: ["/((?!_next|api|favicon.ico).*)"],
 };
