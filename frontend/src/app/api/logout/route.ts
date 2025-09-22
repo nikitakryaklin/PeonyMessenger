8af9200522d29b9ed93b2772fdd52615f58fe84c
nikitakryaklin <nikitakryaklin2013@gmail.com>
2025-09-22 11:20:45 +0300

    fix(loguot): change types

--- frontend/src/app/api/logout/route.ts
+++ frontend/src/app/api/logout/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server";
 
-export async function POST(req: Response) {
+export async function POST(req: Request) {
   // const response = NextResponse.json;
 
   const res = NextResponse.redirect(new URL("/", req.url));
