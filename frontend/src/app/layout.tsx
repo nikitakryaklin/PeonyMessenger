81725ba09a12e16bd000ea0f283965d4a1410844
nikitakryaklin <nikitakryaklin2013@gmail.com>
2025-09-24 00:13:57 +0300

    fix(seo): add right icons and metadata

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -2,8 +2,6 @@ import type { Metadata } from "next";
 import { DM_Sans } from "next/font/google";
 import "./globals.css";
 import { MainPrivider } from "./_mainPrivider";
-import { Header } from "@/widgets";
-import { icons } from "lucide-react";
 
 const DMSans = DM_Sans({
   variable: "--font-dm-sans",
@@ -13,16 +11,28 @@ const DMSans = DM_Sans({
 
 export const metadata: Metadata = {
   title: "Peony",
-  description: "Peony - the beast messanger",
+  description: "Peony - the best messenger",
   openGraph: {
-    images: "/icon/logo-180x180.svg",
+    title: "Peony",
+    description: "Peony - the best messenger",
+    images: [{ url: "/icon/favicon-180x180.svg" }],
   },
   icons: {
     icon: [
-      { url: "/icon/logo-16x16.png", sizes: "16x16", type: "image/png" },
-      { url: "/icon/logo-32x32.png", sizes: "32x32", type: "image/png" },
+      { url: "/icon/favicon.ico" },
+      { url: "/icon/favicon-16x16.png", sizes: "16x16", type: "image/png" },
+      { url: "/icon/favicon-32x32.png", sizes: "32x32", type: "image/png" },
+    ],
+    apple: [{ url: "/icon/apple-touch-icon.png", sizes: "180x180" }],
+    other: [
+      {
+        rel: "mask-icon",
+        url: "/icon/apple-touch-icon.png",
+        color: "#e0b8ff",
+      },
     ],
   },
+  manifest: "/site.webmatifest",
 };
 
 export default function RootLayout({
